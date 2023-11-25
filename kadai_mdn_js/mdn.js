93a6a071f2359c217260094f4da0107fcd0aa6a5
shimishoooo <127800611+shimishoooo@users.noreply.github.com>
2023-11-26 08:44:53 +0900

    課題提出
    JavaScriptの基礎を学ぼうの課題提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const NowDate = new Date();
+const NowYear = NowDate.getFullYear();
+const NowMonth = NowDate.getMonth() + 1;
+const NowDay = NowDate.getDate();
+
+console.log(NowYear + "年" + NowMonth + "月" + NowDay + "日")
+
